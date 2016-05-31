2b99019a53e1319c4c177e61f2442c94b236364f
GhislainRaze <gharz@hotmail.com>
2016-05-31 19:47:31 +0200

    Add files via upload

---
+++ TopOpt/Discretization/MonomialBasis.m
@@ -0,0 +1,34 @@
+%% Monomial Basis
+%
+% Created by J.T.B. Overvelde - 18 April 2012
+%
+% Master's thesis - The Moving Node Approach in Topology Optimization
+%
+% <http://www.overvelde.com>
+%
+% Monomial basis at coordinate x for pn terms.
+
+function [p dpdx dpdy]=MonomialBasis(x,pn)
+ 
+if pn==1
+    p=1;
+    dpdx=0;
+    dpdy=0;
+elseif pn==2
+    p=[1 x(1) x(2)]';
+    dpdx=[0 1 0]';
+    dpdy=[0 0 1]';
+elseif pn==3
+    p=[1 x(1) x(2) x(1)^2 x(2)^2 x(1)*x(2)]';
+    dpdx=[0 1 0 2*x(1) 0 x(2)]';
+    dpdy=[0 0 1 0 2*x(2) x(1)]';
+elseif pn==4
+    p=[1 x(1) x(2) x(1)^2 x(2)^2 x(1)*x(2) x(1)^3 x(2)^3 x(1)^2*x(2) x(1)*x(2)^2]';
+    dpdx=[0 1 0 2*x(1) 0 x(2) 3*x(1)^2 0 2*x(1)*x(2) x(2)^2]';
+    dpdy=[0 0 1 0 2*x(2) x(1) 0 3*x(2)^2 x(1)^2 2*x(1)*x(2)]';
+else
+    disp('chose value for pn between 1 and 4')
+end
+        
+
+    
---
+++ TopOpt/Discretization/WeightTensor.m
@@ -0,0 +1,44 @@
+%% Weight Tensor function
+% 
+% Created by J.T.B. Overvelde - 18 April 2012
+%
+% Master's thesis - The Moving Node Approach in Topology Optimization
+%
+% <http://www.overvelde.com>
+%
+% Kernel function used in the MLS shape function for node x at coordinate
+% xi.
+
+function [w dwdx dwdy]=WeightTensor(xj,xi,dm)
+
+alphax=2/dm(1);
+alphay=2/dm(2);
+rx = abs(xj(1)-xi(1))/dm(1);
+ry = abs(xj(2)-xi(2))/dm(2);
+drdx = sign(xj(1)-xi(1))/dm(1);
+drdy = sign(xj(2)-xi(2))/dm(2);
+if rx>1
+    wx = 0;
+    dwdrx = 0;
+elseif rx>0.5
+    wx = alphax*(4/3-4*rx+4*rx^2-4/3*rx^3);
+    dwdrx = alphax*(-4+8*rx-4*rx^2)*drdx;
+else
+    wx = alphax*(2/3-4*rx^2+4*rx^3);
+    dwdrx = alphax*(-8*rx+12*rx^2)*drdx;
+end
+if ry>1
+    wy = 0;
+    dwdry = 0;
+elseif ry>0.5
+    wy = alphay*(4/3-4*ry+4*ry^2-4/3*ry^3);
+    dwdry = alphay*(-4+8*ry-4*ry^2)*drdy;
+else
+    wy = alphay*(2/3-4*ry^2+4*ry^3);
+    dwdry = alphay*(-8*ry+12*ry^2)*drdy;
+end
+
+w=wx*wy;
+dwdx=wy*dwdrx;
+dwdy=wx*dwdry;
+
