8c77a9fb07d3f90e5949dc0bdbbe5161eebb6795
GhislainRaze <gharz@hotmail.com>
2016-06-01 11:50:38 +0200

    Add files via upload

--- TopOpt/Constants/GlobalConst.m
+++ TopOpt/Constants/GlobalConst.m
@@ -1,9 +1,26 @@
-%Created by J.T.B. Overvelde - 18 April 2012
-%Master's thesis - The Moving Noda Approach in Topology Optimization
-%http://www.overvelde.com
+%% Global Constants
 %
-%Makes some constants global.
+% Created by J.T.B. Overvelde - 18 April 2012
+%
+% Master's thesis - The Moving Node Approach in Topology Optimization
+%
+% <http://www.overvelde.com>
+%
+% Makes some constants global. This includes
+%
+% * _pCon_: the problem constants
+% * _mCon_: the discretization constants
+% * _mmCon_: the material distribution constants
+% * _bcCon_: the boundary conditions constants
+%
+% * _nodes_: the nodes constants
+% * _mnodes_: the mass nodes constants
+% * _bnodes_: the boundary nodes constants
+% * _cells_: the cells (EFG)/elements (FEM) constants
+% * _bcells_: the boundary cells constants (only for EFG)
+%
+% * _mTot_: the total mass of the structure
 
-global pCon mCon mmCon sCon bcCon
-global nodes mnodes bnodes cells bcells solnodes 
+global pCon mCon mmCon bcCon
+global nodes mnodes bnodes cells bcells 
 global mTot
