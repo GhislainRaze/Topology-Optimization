bb3443185ac421f5de9449a5a262adf587853a62
GhislainRaze <gharz@hotmail.com>
2016-06-06 11:10:13 +0200

    Add files via upload
    Added filter

--- TopOpt/EFG/EFG.m
+++ TopOpt/EFG/EFG.m
@@ -19,6 +19,8 @@
 % * _G_: the lagrangian multipliers matrix
 % * _q_: the second member associated with lagrangian multipliers
 % * _distrType_: the material distribution type
+% * _H_: the filter convolution matrix (optional)
+% * _Hs_: the sum of the filter convolution matrix lines (optional)
 %
 % If they are not specified, they are computed from EFGUnitMatrices
 %
@@ -38,7 +40,7 @@
 
 
 function [ug,Compliance,dCdx,mTot,tk,tm,tdK,time1,time2,time3]=...
-    EFG(Ke,f,G,q,distrType)
+    EFG(Ke,f,G,q,distrType,H,Hs)
 
 GlobalConst
 if nargin < 4
@@ -46,6 +48,9 @@ if nargin < 4
 end
 if nargin < 5
     distrType = 1;
+end
+if nargin < 6
+    filter = false;
 end
 tic %Assembly timer
 
@@ -63,6 +68,16 @@ mTot = 0;
 tk = 0;
 tm = 0;
 tdK = 0;
+
+if filter
+    % If there is a filter, the densities are first computed and saved,
+    % then filtered, then the matrices are assembled.
+    massNodes = 1:nd*mmCon.n;
+    rhoVec = zeros(mCon.m*mCon.nG^2,1);
+    drhoVec = zeros(mCon.m*mCon.nG^2,nd*mmCon.n);
+end
+
+
 for ic=1:mCon.m                                         % Iterations over the internal cells
     for ip=1:cells(ic).ni                               % Iterations over the cell Gauss points
         if ~isempty(cells(ic).int(ip).nen)
@@ -73,26 +88,56 @@ for ic=1:mCon.m                                         % Iterations over the in
             for i = 1:nd
                 emn(i:nd:end-nd+i)=nd*[cells(ic).int(ip).nemn]-nd+i;
             end
-            [rho,drhoAdx] = asymptoticDensity(cells(ic).int(ip).x,...
+            [rho,drhodx] = asymptoticDensity(cells(ic).int(ip).x,...
                 [mnodes(cells(ic).int(ip).nemn).x],...
                 [mnodes(cells(ic).int(ip).nemn).theta],...
                 [mnodes(cells(ic).int(ip).nemn).l]/2,...
                 [mnodes(cells(ic).int(ip).nemn).m],...
                 mmCon.rhoMin,mmCon.rhoMax,distrType);
             tic
-            K(en,en)=K(en,en)+rho^mmCon.p*Ke{(ic-1)*mCon.nG^2+ip};
+            if filter
+                rhoVec((ic-1)*mCon.nG^2+ip) = rho;
+            else
+                K(en,en)=K(en,en)+rho^oCon.p*Ke{(ic-1)*mCon.nG^2+ip};
+            end
             tk = tk+toc;
             tic
             mTot = mTot + rho*cells(ic).J*cells(ic).int(ip).w;
             tm = tm+toc;
             tic
-            for i=1:length(emn)
-                dKdx{emn(i)}(en,en)=dKdx{emn(i)}(en,en)+...
-                    sparse(mmCon.p*drhoAdx(i)*rho^(mmCon.p-1)*Ke{(ic-1)*mCon.nG^2+ip}); 
+            if filter
+                drhoVec((ic-1)*mCon.nG^2+ip,emn) = drhodx;
+            else
+                for i=1:length(emn)
+                    dKdx{emn(i)}(en,en)=dKdx{emn(i)}(en,en)+...
+                        sparse(oCon.p*drhodx(i)*rho^(oCon.p-1)*Ke{(ic-1)*mCon.nG^2+ip}); 
+                end
             end
             tdK = tdK + toc;
         end
     end
+end
+
+if filter
+    % Filtering
+    drhoVec = sparse(drhoVec);
+    rhoVec = (H*rhoVec)./Hs;
+    drhoVec = (H*drhoVec)./Hs;
+    
+    % Assembly
+    for i = 1 : length(rhoVec)
+        ic = ceil(i/mCon.nG^2);
+        ip = mod(i-1,mCon.nG^2)+1;
+        en=zeros(1,2*length(cells(ic).int(ip).nen));
+        en(1:2:end-1)=2*[cells(ic).int(ip).nen]-1;      % x index of neighboring cells
+        en(2:2:end)=2*[cells(ic).int(ip).nen];          % y index
+        K(en,en)=K(en,en)+rhoVec(i)^oCon.p*Ke{(ic-1)*mCon.nG^2+ip};
+        emn = massNodes(drhoVec ~= 0);
+        for j=1:length(emn)
+            dKdx{emn(j)}(en,en)=dKdx{emn(j)}(en,en)+...
+                sparse(oCon.p*drhoVec(i,j)*rho^(oCon.p-1)*Ke{(ic-1)*mCon.nG^2+ip}); 
+        end
+   end
 end
 
 
