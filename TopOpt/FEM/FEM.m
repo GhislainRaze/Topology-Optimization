4ef6fd1e3d3fffb2d6cf8192751a74405d5510b4
GhislainRaze <gharz@hotmail.com>
2016-06-06 11:09:56 +0200

    Add files via upload
    Added filter

--- TopOpt/FEM/FEM.m
+++ TopOpt/FEM/FEM.m
@@ -19,6 +19,8 @@
 % * _ubar_: the imposed nodal displacements (set to NaN if there is no
 % imposed displacement associated to a given node)
 % * _distrType_: the mass distribution type
+% * _H_: the filter convolution matrix (optional)
+% * _Hs_: the sum of the filter convolution matrix lines (optional)
 %
 % If they are not specified, they are computed from FEMUnitMatrices
 %
@@ -38,7 +40,7 @@
 
 
 function [ug,Compliance,dCdx,mTot,tk,tm,tdK,time1,time2,time3]=...
-    FEM(Ke,f,ubar,distrType)
+    FEM(Ke,f,ubar,distrType,H,Hs)
 
 GlobalConst
 if nargin < 3
@@ -46,6 +48,9 @@ if nargin < 3
 end
 if nargin < 4
     distrType = 1;
+end
+if nargin < 6
+    filter = false;
 end
 
 
@@ -64,6 +69,14 @@ tk = 0;
 tm = 0;
 tdK = 0;
 
+if filter
+    % If there is a filter, the densities are first computed and saved,
+    % then filtered, then the matrices are assembled.
+    massNodes = 1:nd*mmCon.n;
+    rhoVec = zeros(mCon.m*mCon.nG^2,1);
+    drhoVec = zeros(mCon.m*mCon.nG^2,nd*mmCon.n);
+end
+
 %% Assembly K matrix and f vector internal cells
 tic %Assembly timer
 for ic=1:mCon.m                                         % Iterations over the internal cells
@@ -75,27 +88,61 @@ for ic=1:mCon.m                                         % Iterations over the in
         for i = 1:nd
             emn(i:nd:end-nd+i)=nd*[cells(ic).int(ip).nemn]-nd+i;
         end
-        [rho,drhoAdx] = asymptoticDensity(cells(ic).int(ip).x,...
+        [rho,drhodx] = asymptoticDensity(cells(ic).int(ip).x,...
             [mnodes(cells(ic).int(ip).nemn).x],...
             [mnodes(cells(ic).int(ip).nemn).theta],...
             [mnodes(cells(ic).int(ip).nemn).l]/2,...
             [mnodes(cells(ic).int(ip).nemn).m],...
             mmCon.rhoMin,mmCon.rhoMax,distrType);
         tic
-        K(en,en)=K(en,en)+rho^mmCon.p*Ke{(ic-1)*mCon.nG^2+ip};
+        if oCon.filter
+            rhoVec((ic-1)*mCon.nG^2+ip) = rho;
+        else
+            K(en,en)=K(en,en)+rho^oCon.p*Ke{(ic-1)*mCon.nG^2+ip};
+        end
         tk = tk+toc;
         tic
         mTot = mTot + rho*cells(ic).J*cells(ic).int(ip).w;
         tm = tm+toc;
         tic
-        for i=1:length(emn)
-            dKdx{emn(i)}(en,en)=dKdx{emn(i)}(en,en)+...
-                sparse(mmCon.p*drhoAdx(i)*rho^(mmCon.p-1)*Ke{(ic-1)*mCon.nG^2+ip}); 
+        if oCon.filter
+            drhoVec((ic-1)*mCon.nG^2+ip,emn) = drhodx;
+        else
+            for i=1:length(emn)
+                dKdx{emn(i)}(en,en)=dKdx{emn(i)}(en,en)+...
+                    sparse(oCon.p*drhodx(i)*rho^(oCon.p-1)*Ke{(ic-1)*mCon.nG^2+ip}); 
+            end
         end
         tdK = tdK + toc;
     end
 end
 
+if oCon.filter
+    % Filtering
+    drhoVec = sparse(drhoVec);
+    rhoVec = (H*rhoVec)./Hs;
+    for i = 1 : size(drhoVec,2)
+        drhoVec(:,i) = H*(drhoVec(:,i)./Hs);
+    end
+    
+    % Assembly
+    for i = 1 : length(rhoVec)
+        ic = ceil(i/mCon.nG^2);
+        ip = mod(i-1,mCon.nG^2)+1;
+        en=zeros(1,2*length(cells(ic).nen));
+        en(1:2:end-1)=2*[cells(ic).nen]-1;      % x index of neighboring cells
+        en(2:2:end)=2*[cells(ic).nen];          % y index
+        K(en,en)=K(en,en)+rhoVec(i)^oCon.p*Ke{(ic-1)*mCon.nG^2+ip};
+        emn=zeros(1,nd*length(cells(ic).int(ip).nemn));
+        for j = 1:nd
+            emn(j:nd:end-nd+j)=nd*[cells(ic).int(ip).nemn]-nd+j;
+        end
+        for j=1:length(emn)
+            dKdx{emn(j)}(en,en)=dKdx{emn(j)}(en,en)+...
+                sparse(oCon.p*drhoVec(i,emn(j))*rhoVec(i)^(oCon.p-1)*Ke{(ic-1)*mCon.nG^2+ip}); 
+        end
+   end
+end
 
 time1=toc; %Assembly timer
 
